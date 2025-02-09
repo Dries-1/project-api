3236e911e49f558a66e50db819a123b702d01cfc
Dries-1 <dries-vdw@outlook.com>
2025-02-09 21:11:51 +0100

    klaar

--- main.py
+++ main.py
@@ -22,31 +22,24 @@ def get_humidity(response):
 def get_weather_description(response):
     return response['weather'][0]['description']
 
-def get_sunrise_time(response):
-    return dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone']).strftime('%H:%M:%S')
-
-def get_sunset_time(response):
-    return dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone']).strftime('%H:%M:%S')
-
 if __name__ == "__main__":
     API_key = "8467dccc7c973e4c667fc1ceb1e57b2c"
     
     city_name = input("Voor welke stad wil je het weer opvragen? ")
     weather_data = get_weather_data(city_name, API_key)
     
-    vraag = input("Welke informatie wil je zien? (temperatuur, windsnelheid, luchtvochtigheid) ")
+    vraag = input("Welke informatie wil je zien? temperatuur(1), windsnelheid(2), luchtvochtigheid(3) : ")
     
-    if vraag.lower() == "temperatuur":
+    if vraag.lower() in ["temperatuur", "1"]:
         print(f"De temperatuur in {city_name} is {get_temperature(weather_data):.2f}°C")
-    elif vraag.lower() == "windsnelheid":
+    elif vraag.lower() in ["windsnelheid", "2"]:
         print(f"De windsnelheid in {city_name} is {get_wind_speed(weather_data)} m/s")
-    elif vraag.lower() == "luchtvochtigheid":
+    elif vraag.lower() in ["luchtvochtigheid", "3"]:
         print(f"De luchtvochtigheid in {city_name} is {get_humidity(weather_data)}%")
     else:
         print("Ongeldige keuze.")
     
-    extra_vraag = input("Wil je extra details zoals zonsopkomst en zonsondergang? (ja/nee) ")
+    extra_vraag = input("Wil je de weersomschrijving? (ja/nee) ")
     if extra_vraag.lower() == "ja":
         print(f"Het weer in {city_name} wordt omschreven als: {get_weather_description(weather_data)}")
-        print(f"De zonsopkomst in {city_name} is om {get_sunrise_time(weather_data)} UTC")
-        print(f"De zonsondergang in {city_name} is om {get_sunset_time(weather_data)} UTC")
+
