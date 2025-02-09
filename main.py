aadc6027b463d2b838709be09291a47bc3489218
Dries-1 <dries-vdw@outlook.com>
2025-02-09 20:38:23 +0100

    Refactor temperature conversion function in main.py

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 API_key = "8467dccc7c973e4c667fc1ceb1e57b2c"
 city_name = "New York"
 
+def kelvin_to_celsius(temp):
+    celcius = temp - 273.15
+    return celcius
 
 complete_url = f"{base_url}appid={API_key}&q={city_name}"
 
 response = requests.get(complete_url).json()
 print(response)
-
+ 
